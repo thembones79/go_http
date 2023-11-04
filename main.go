1a01203e7e049d984d347f333c3e3aa796b841f6
thembones79 <michal.szumnarski@gmail.con>
2023-11-04 12:02:54 +0100

    interface work

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
-func main(){
-    resp, err := http.Get("http://google.com")
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
+type logWriter struct{}
 
-    fmt.Println(resp)
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	io.Copy(os.Stdout, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	return 1, nil
 }
